3dda0992116d32ef3b74891db382a3cfa5c049f9
Omoyeni Oreoluwani Precious <omongprec@gmail.com>
2023-01-08 00:39:56 +0100

    Data Structures

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print("")
+    for x in range(len(matrix)):
+        if len(matrix[x]) == 0:
+            print("")
+        for y in range(len(matrix[x])):
+            if y != len(matrix[x]) - 1:
+                print("{:d}".format(matrix[x][y]), end=" ")
+            else:
+                print("{:d}".format(matrix[x][y]))
