b135da2740062e7ed0736a114ca92ae83f86e287
Omoyeni Oreoluwani <omongprec@gmail.com>
2024-01-12 22:40:21 +0100

    Loops

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         char_num = ord(i)
         if (char_num >= 97 and char_num <= 122):
-            print("{}".format(chr(char_num - 32)), end="")
-        else:
-            print("{}".format(chr(char_num)), end="")
+            char_num = char_num - 32
+        print("{}".format(chr(char_num)), end="")
+    print("")
