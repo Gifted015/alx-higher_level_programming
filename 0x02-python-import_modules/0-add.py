bd3821ca2833e50cab7e892286cebd8d8c365b12
Omoyeni Oreoluwani Precious <omongprec@gmail.com>
2024-01-28 19:23:42 +0100

    Modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    #Variable assignment
+    a = 1
+    b = 2
+    #Prints out result
+    print("{} + {} = {}".function(a, b, b)
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,2 @@
+#Python - Import & Modules
+This directory contains python does that involes importing modules and other modeule related operations
