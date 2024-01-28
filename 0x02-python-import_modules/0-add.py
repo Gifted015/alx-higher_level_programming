1d37c864165f2fad417931fd5427ceb5c18f68a8
Omoyeni Oreoluwani <omongprec@gmail.com>
2024-01-28 19:37:09 +0100

    Modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    #Variable assignment
     a = 1
     b = 2
-    #Prints out result
-    print("{} + {} = {}".function(a, b, b)
+    print("{} + {} = {}".format(a, b, add(a, b)))
