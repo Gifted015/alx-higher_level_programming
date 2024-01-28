cc29936be879f4892e6621dfda2598b42e24ec1a
Omoyeni Oreoluwani <omongprec@gmail.com>
2024-01-28 20:47:11 +0100

    Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,12 @@ if __name__ == "__main__":
     arguments = sys.argv
     num_arg = len(arguments)
 
-    if (num_arg == 0):
-        print("0 argument.")
+    if (num_arg == 1):
+        print("0 arguments.")
     else:
-        if (num_arg == 1):
+        if (num_arg == 2):
             print("1 argument:")
         else:
-            print("{} arguments:".format(num_arg))
-        for i in range(0, num_arg):
-            print("{}: {}".format(arguments[i]))
+            print("{} arguments:".format(num_arg - 1))
+        for i in range(1, num_arg):
+            print("{}: {}".format(i, arguments[i]))
