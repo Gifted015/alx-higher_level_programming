201ee5deaddfbeeb7e7d835b06b4c6f44e65fa48
Omoyeni Oreoluwani <omongprec@gmail.com>
2024-01-28 22:41:28 +0100

    Modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     num = sys.argv
     sum = 0
-    for i in num:
-        i = int(i)
+    for i in range(1, len(num)):
+        i = int(num[i])
         sum += i
     print("{}".format(sum))
