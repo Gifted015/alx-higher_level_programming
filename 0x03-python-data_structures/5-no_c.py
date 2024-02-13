35b5ebc023cb628562796e4dba862c6de399ce7c
Omoyeni Oreoluwani <omongprec@gmail.com>
2024-02-13 16:59:19 +0100

    Data Structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,8 +4,8 @@ def no_c(my_string):
     i = 0
     str_len = len(str_list)
     while (i < str_len):
-        if (str_list[i] == 'c' || str_list[i] == 'C'):
-            str_list.pop(i)
+        if (str_list[i] == 'c' or str_list[i] == 'C'):
+            str_list.remove(str_list[i])
             str_len -= 1
         i += 1
     new_str = "".join(str_list)
